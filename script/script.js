a9fa6a8f34c3a32350be7ef6bc81ef5de7e137f2
NovAldesign <williams.lavon@gmail.com>
2026-01-06 22:38:12 -0500

    select html elements in js

---
+++ script/script.js
@@ -0,0 +1,11 @@
+//caching the main display
+const display = document.getElementById('display');
+
+//caching form 
+const goalForm = document.getElementById('goal');
+
+//caching all mode buttons at once
+const modeButtons = document.querySelectorAll('mode');
+
+//caching history list
+const logList = document.getElementById('log');
